27dcbf90b7de349faccd839a058f0d9f37081017
jy03147937 <persistence@vip.qq.com>
2018-05-14 23:39:57 +0800

    Add a tab Blog

---
+++ WebDemo/ClientApp/components/blog.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class Blog extends React.Component<RouteComponentProps<{}>, {}> {
+    public render() {
+        return <div>
+            <h1>Hello, world!</h1>
+        </div>;
+    }
+}
