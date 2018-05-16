239a5daffe0009d39113fa7ec5d32e2f01cdbb56
jy03147937 <persistence@vip.qq.com>
2018-05-16 23:37:37 +0800

    Add a very simple real time preview markdown editor to Blog tab

--- WebDemo/ClientApp/components/blog.tsx
+++ WebDemo/ClientApp/components/blog.tsx
@@ -1,10 +1,42 @@
 ﻿import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import * as marked from 'marked'
 
-export class Blog extends React.Component<RouteComponentProps<{}>, {}> {
+interface BlogState {
+    previewContent: string
+}
+
+export class Blog extends React.Component<RouteComponentProps<{}>, BlogState> {
+    constructor() {
+        super();
+        this.state = {
+            previewContent: ''
+        };        
+    }
+
     public render() {
+
+        const previewContent = this.state.previewContent;
+
         return <div>
-            <h1>Hello, world!</h1>
-        </div>;
+            <header key='header'>
+                <textarea type="text" placeholder="输入文章标题..." onInput={e => { this.onContentChange(e) }}/>
+            </header>,
+            <div key='main'>
+                <div>
+                    <div dangerouslySetInnerHTML={{ __html: previewContent }} />
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    }
+
+    onContentChange(e: React.FormEvent<HTMLTextAreaElement>) {
+        console.log(e.currentTarget.value)
+        this.setState({
+            previewContent: marked(e.currentTarget.value, { breaks: true })
+        })
     }
 }
